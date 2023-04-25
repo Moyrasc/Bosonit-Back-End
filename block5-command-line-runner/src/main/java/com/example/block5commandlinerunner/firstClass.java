b409b282c951a7e7aec0e3c641312d3f2567e695
Moyrasc <noemisolac@gmail.com>
2023-04-25 15:30:41 +0200

    create first class

---
+++ block5-command-line-runner/src/main/java/com/example/block5commandlinerunner/firstClass.java
@@ -0,0 +1,14 @@
+package com.example.block5commandlinerunner;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.stereotype.Component;
+//@Component se usa para indicar un componente de Spring,
+// marca la clase como un Bean y así podra agregarla al contexto de la app
+@Component
+public class firstClass {
+    //@PostConstruct se ejecuta una vez antes de la inicialización del bean.
+    @PostConstruct
+    public void firstClass() {
+        System.out.println("Hi from first class");
+    }
+}
